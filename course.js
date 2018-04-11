bb3a812a297f250add7d6bc4019b7639348e5fca
priynka <psidella@gmail.com>
2018-04-10 19:07:41 -0500

    js 2

--- course.js
+++ course.js
@@ -5,3 +5,34 @@ var Course ='Astronomy Physics';
 console.log(Teacher);
 console.log(Semester);
 console.log(Course);
+var coursearray = [
+['Astronomy', 'Physics1'],
+['Astronomy', 'Physics1'],
+['Astronomy', 'Physics2'],
+['Astronomy', 'Physics2'],
+['Astronomy', 'Physics2']
+];
+
+function newdept(coursearray,department){
+var newArray = [];
+  for(i=0;i<5;i++){
+    for (j=1;j<2;j++){
+      if(coursearray[i][j] ==department ) newArray.push(coursearray[i]);
+    }
+  }
+ return newArray;
+}
+
+var department;
+var cond = true;
+
+do{
+department = window.prompt('What department are looking for a course in');
+if ((department == 'Physics1') || (department == 'Physics2')){
+  cond = false;
+}
+}while(cond);
+
+var newArray =newdept(coursearray,department);
+window.alert('You Can choose  courses from department ' + department);
+console.log(newArray);
--- teacher.js
+++ teacher.js
@@ -14,3 +14,35 @@ var avgRating;
 avgRating = (Rating1+Rating2+Rating3)/3;
 avgRating = Math.round( avgRating * 10) / 10;
 console.log(avgRating);
+
+// CG Education - Javascript Part II  - Below
+var ratings =[3.4,5.0,4.2];
+
+console.log(ratings);
+
+function getRatingAvg(ratings){
+var avgRating;
+avgRating = (ratings[0]+ratings[1]+ratings[2]+ratings[3])/4;
+return  avgRating;
+console.log(avgRating);
+}
+
+function addTeacherRating(ratings,newrating){
+ratings.push(newrating);
+//Return ratings;
+console.log(ratings);
+}
+var rating;
+var cond = true;
+do{
+  rating = window.prompt('We would like for you to review . Please enter a rating between 0.0 - 5.0?');
+rating =Number(rating);
+  if(rating <=5){
+    cond = false;
+  }
+}while (cond);
+
+addTeacherRating(ratings,rating);
+newrating = getRatingAvg(ratings);
+window.alert('Thanks for you review! '+ teacher +' average rating is now  .'+newrating);
+console.log(newrating);
